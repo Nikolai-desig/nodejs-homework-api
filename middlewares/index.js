675721ea00f7e0b08aa65429aa65156d3b922ed6
Nikolai-desig <rinzleramad79@gmail.com>
2023-07-31 19:38:09 +0300

    hw5_avatar_add

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isValidId = require("./isValidId");
 const isValidUser = require("./isValidUser");
 const authenticate = require("./authenticate");
+const upload = require("./upload")
 
 module.exports = {
   isValidId,
   isValidUser,
   authenticate,
+  upload, 
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/auth");
 
 const { isValidUser } = require("../../middlewares");
 
-const { authenticate } = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 
 const router = express.Router();
 
@@ -17,6 +17,8 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch("/:id/subscription", authenticate, isValidUser, ctrl.updateSubscriptionUser);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
 
 module.exports = router;
 
